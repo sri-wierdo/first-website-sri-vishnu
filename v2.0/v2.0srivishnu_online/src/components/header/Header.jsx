40a1ed968628fe5f06146dadbba01514cab1bda3
sri_wierdo <mannemsrivishnureddy@gmail.com>
2022-05-15 13:46:33 +0530

    Do responsive Header Section that works for all devices including Social media icons email me button and all other CTA's location of the scroll down button is set heder section is complete

--- v2.0/v2.0srivishnu_online/src/components/header/Header.jsx
+++ v2.0/v2.0srivishnu_online/src/components/header/Header.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+import HeaderEmail from './HeaderEmail'
 const Header = () => {
   return (
     <header>
-      <div className="container header__cointainer">
+      <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Sri Vishnu</h1>
-        <h5 className="text-light">Fullstack Develloper</h5>
+        <h5 className="text-light">I'm a Fullstack Develloper </h5>
         <CTA />
+        <HeaderSocials />
+        <HeaderEmail />
         <div className="me">
           <img src={ME} alt="a beautiful handsome bald gentleman" />
         </div>
 
-        <a href="#contact" className="scroll__down">Go to the botom of Page</a>
+        <a href="#contact" className="scroll__down">Scroll all the way down</a>
 
       </div>
     </header>
